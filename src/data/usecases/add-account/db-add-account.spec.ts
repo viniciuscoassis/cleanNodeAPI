dd81464fe3c42775dba17c120c6571172cdf46ee
viniciuscoassis <vinicius.coassis@gmail.com>
2023-07-17 16:16:10 -0300

    refactor: move documents

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,5 @@
 import { DbAddAccount } from './db-add-account'
-import { Encrypter } from './protocols/encrypter'
+import { Encrypter } from '../../protocols/encrypter'
 
 interface SutTypes {
   sut: DbAddAccount
